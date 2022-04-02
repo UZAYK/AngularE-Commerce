2063300dd779eb5cb929446b880dc3788df44c1f
Uzay KAHRAMAN <uzaykahramann@gmail.com>
2022-04-03 00:40:30 +0300

    Add project files.

---
+++ API.Data/DataContext/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Text;
+using API.Data.DbModels;
+
+namespace API.Data.DataContext
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options): base (options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data.DataContext;
+using API.Data.DbModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+        public ProductsController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var data = await _storeContext.Products.ToListAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProducts(int id) => await _storeContext.Products.FindAsync(id);
+
+    }
+}
